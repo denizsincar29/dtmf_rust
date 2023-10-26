fbd955bfb678b6d5507d08195899d78b4440e72d
Deniz Sincar <dsincar29@gmail.com>
2023-10-26 19:26:54 +0300

    made cargo make script.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use std::sync::mpsc::channel;
-use pv_recorder::PvRecorderBuilder;
-use dtmf::{decoder::{Decoder, ToneChange}, enums::{State, Tone}};
 use dasp::sample::conv;
+use dtmf::{
+    decoder::{Decoder, ToneChange},
+    enums::{State, Tone},
+};
+use pv_recorder::PvRecorderBuilder;
+use std::sync::mpsc::channel;
 
 struct Tones {
     tone: char,
@@ -9,58 +12,71 @@ struct Tones {
 }
 impl ToneChange for Tones {
     fn tone_change(&mut self, tone: Tone, state: State) {
-        match state{
-            State::On => {self.tone=tone.as_char(); self.tones.push(self.tone);}
-            State::Off => {self.tone='n'}
+        match state {
+            State::On => {
+                self.tone = tone.as_char();
+                self.tones.push(self.tone);
+            }
+            State::Off => self.tone = 'n',
         }
     }
 }
 
 struct Detector {
-    samplerate: u32,
-    decoder: Decoder<Tones>
+    decoder: Decoder<Tones>,
 }
 
 impl Detector {
     fn new(sr: u32) -> Self {
-        Self{samplerate: sr, decoder: Decoder::new(sr, Tones { tone: 'n', tones: "".to_string() })}
+        Self {
+            decoder: Decoder::new(
+                sr,
+                Tones {
+                    tone: 'n',
+                    tones: "".to_string(),
+                },
+            ),
+        }
     }
-    fn last_tone(&self) -> char {self.decoder.tone_change.tone}
-    fn tones(&self) -> String {self.decoder.tone_change.tones.clone()}
-    
-    
+    fn last_tone(&self) -> char {
+        self.decoder.tone_change.tone
+    }
+    fn tones(&self) -> String {
+        self.decoder.tone_change.tones.clone()
+    }
+
     fn decode(&mut self, data: Vec<f32>) {
         self.decoder.process(&data);
     }
 }
 
-
-
-
 fn main() {
     println!("DTMF decoder, v1.0 by Deniz Sincar.");
-    let (tx, rx)=channel();
+    let (tx, rx) = channel();
     ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel.")).unwrap();
     let frame_length = 512;
-    let recorder = PvRecorderBuilder::new(frame_length).init().expect("can't init!");
-    let mut det=Detector::new(recorder.sample_rate() as u32);
+    let recorder = PvRecorderBuilder::new(frame_length)
+        .library_path(std::path::Path::new("libpv_recorder.dll"))
+        .init()
+        .expect("can't init!");
+    let mut det = Detector::new(recorder.sample_rate() as u32);
     recorder.start().expect("cannot start rec!");
     println!("Recorder started. Ready to decode DTMF.");
-    let mut ot='n';
+    let mut ot = 'n';
     while recorder.is_recording() {
         let frame = recorder.read().expect("cannot read!");
-        let convframe: Vec<f32>=frame.iter().map(|s| {conv::i16::to_f32(*s)}).collect();
+        let convframe: Vec<f32> = frame.iter().map(|s| conv::i16::to_f32(*s)).collect();
         det.decode(convframe);
-        let t=det.last_tone();
-        if t!=ot {
-            if t!='n' {println!("({})", t);}
-            ot=t;
+        let t = det.last_tone();
+        if t != ot {
+            if t != 'n' {
+                println!("({})", t);
+            }
+            ot = t;
         }
         if let Ok(_) = rx.try_recv() {
             recorder.stop().unwrap();
             println!("exitting");
         }
     }
-    
-    
 }
